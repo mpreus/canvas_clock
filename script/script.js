a9ac26b3da9739a9c595f980889ec0642668c5a0
Maciej Preus <mpreus@onet.eu>
2019-11-16 20:04:54 +0100

    Add JavaScript code to see the clock face, hands and get the current time

--- script/script.js
+++ script/script.js
@@ -10,6 +10,99 @@ function init() {
 	/* przesunięcie, by środek koła był w środku elementu canvas */
 	context.translate(radius * 2, radius);
 	/* wielkość koła tarczy nieco mniejsza od całości */
-	radius = radius * 0.90
+	radius = radius * 0.90;
+
+
+	drawClock();
+
+	/* funkcja wywołująca */
+	function drawClock() {
+		drawFace(context, radius); /* tarcza zegara */
+		drawNumbers(context, radius); /* liczby na tarczy zagara */
+		drawTime(context, radius); /* położenie wskazówek zegara */
+	}
+
+	/* funkcja rysująca tarczę zegara */
+	function drawFace(context, radius) {
+		let grad;
+
+		context.beginPath();
+
+  		context.arc(0, 0, radius, 0, 2 * Math.PI);
+  		context.fillStyle = '#fff';
+  		context.fill();
+
+  		grad = context.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.1);
+  		/* obwódka zegara dla efektu 3D */
+  		grad.addColorStop(0, '#451111');
+  		grad.addColorStop(0.5, '#fff');
+  		grad.addColorStop(1, '#451111');
+  		context.strokeStyle = grad; 
+  		context.lineWidth = radius * 0.12; /* szerokość obwódki */
+  		context.stroke();
+  		/* punkt środkowy zegara */
+  		context.beginPath();
+  		context.arc(0, 0, radius * 0.06, 0, 2 * Math.PI);
+  		context.fillStyle = '#451111';
+  		context.fill();
+	}
+
+	/* funkcja rysująca liczby na tarczy zegara */
+	function drawNumbers(context, radius) {
+		let numAngle;
+  		let clockNumber;
+  		/* font dla liczb na tarczy zegara: */
+  		context.font = radius * 0.15 + "px Arial";
+  		/* niewielkie przesunięcie liczb ku środkowi tarczy zegara */
+  		context.textBaseline = "middle";
+  		/* wyśrodkowanie liczb na tarczy: */
+  		context.textAlign = "center";
+  		/* rozmieszczenie liczb wokół tarczy zegara */
+  		for (clockNumber = 1; clockNumber < 13; clockNumber++) {
+   			numAngle = clockNumber * Math.PI / 6;
+    		context.rotate(numAngle);
+    		context.translate(0, -radius * 0.85);
+    		context.rotate(-numAngle);
+    		context.fillText(clockNumber.toString(), 0, 0);
+    		context.rotate(numAngle);
+    		context.translate(0, radius * 0.85);
+    		context.rotate(-numAngle);
+  		}
+
+	}
+
+	/* funkcja definiująca położenie wskazówek zegara */
+	function drawTime(context, radius) {
+		let now = new Date(); 				/* nowa instancja obiektu bieżącego czasu */
+  		let hour = now.getHours(); 			/* bieżąca godzina */
+  		let minute = now.getMinutes();		/* bieżąca minuta */
+  		let second = now.getSeconds();		/* bieżąca sekunda */
+
+  		/* wskazówka godzinowa: */
+  		hour = hour % 12; /* liczba całkowita podzialna przez 12 */
+  		hour = (hour * Math.PI / 6) + ( minute * Math.PI / (6 * 60) ) + ( second * Math.PI / (360 * 60) );
+  		drawHand(context, hour, radius * 0.55, radius * 0.08);
+  		/* wskazówka minutowa: */
+  		minute = (minute * Math.PI / 30) + ( second * Math.PI / (30 * 60) );
+  		drawHand(context, minute, radius * 0.75, radius * 0.06);
+  		/* wskazówka sekundowa: */
+  		second = (second * Math.PI / 30);
+  		drawHand(context, second, radius * 0.85, radius * 0.02);
+	}
+
+	/* funkcja rysująca wskazówki zegara */
+	function drawHand(context, pos, length, width) {
+  		context.beginPath();
+  		context.lineWidth = width;
+  		context.lineCap = "round"; /* okrągłe zakończenie linii */
+  		context.moveTo(0, 0);
+  		context.rotate(pos);
+  		context.lineTo(0, -length);
+  		context.stroke();
+  		context.rotate(-pos);
+	}
+
+
+
 
 }
